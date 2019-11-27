cb4ffbc4a569b3762cdc56f35bd827ae2f8b4653
Lydia Joynes <lydiajoynesgmail.com>
2019-11-27 14:22:38 +0000

    implemented arrShift

--- challenges/week8.js
+++ challenges/week8.js
@@ -48,6 +48,12 @@ const sumArrays = arrs => {
 const arrShift = arr => {
   if (arr === undefined) throw new Error("arr is required");
   // Your code here!
+  if (arr.length >= 2) {
+    [arr[0], arr[arr.length - 1]] = [arr[arr.length - 1], arr[0]]
+    return arr;
+  } else {
+    return arr;
+  }
 };
 
 const findNeedle = (haystack, searchTerm) => {
