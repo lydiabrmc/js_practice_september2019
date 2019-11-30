98e248827cce5854785d1f8db35b58b4b2a06912
Lydia Joynes <lydiajoynesgmail.com>
2019-11-30 12:50:47 +0000

    implemented test for isValidDNA

--- test/week9.test.js
+++ test/week9.test.js
@@ -1,7 +1,20 @@
-import { exportAllDeclaration } from "@babel/types"
+const {
+    sumMultiples,
+    isValidDNA
+} = require("../challenges/week9");
 
 describe("sumMultiples", () => {
     test("This function will receive an array of numbers and should return the sum of any numbers which are a multiple of 3 or 5", () => {
         expect(sumMultiples([9, 3, 15, 30, 1, 7])).toBe(57);
     });
 });
+
+describe("isValidDNA", () => {
+    test("This function will receive a string of characters and should return true/false depending on whether it is a valid DNA string. A valid DNA string may contain characters C, G, T or A only.", () => {
+        expect(isValidDNA("CGTA")).toBe(true);
+    });
+
+    test("This function will receive a string of characters and should return true/false depending on whether it is a valid DNA string. A valid DNA string may contain characters C, G, T or A only.", () => {
+        expect(isValidDNA("CEGAT")).toBe(false);
+    });
+});   
